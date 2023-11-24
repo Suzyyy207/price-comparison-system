2c8f1831a4f40186a57a616c1ab6a64bceceb10f
huiyvyao <2422314671@qq.com>
2023-11-24 19:32:29 +0800

    init

---
+++ src/main/java/org/example/NewspringbootApplication.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NewspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspringbootApplication.class, args);
+    }
+
+}
