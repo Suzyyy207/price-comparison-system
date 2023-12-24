523498fc76edcc4b128b362c34e7ba71ce790b6f
suzyyy <suzyyy207@gmail.com>
2023-12-24 19:14:36 +0800

    add yearly report user(some problems)

--- front-end/Price-Check/src/views/user_web/yearly_report.vue
+++ front-end/Price-Check/src/views/user_web/yearly_report.vue
@@ -32,7 +32,43 @@ import top_nav from '../../components/trivial/top_nav.vue'
 
     <!-- 商品列表 -->
     <div class="search-bar">
-      <h2>今年，不同性别的用户喜欢的</h2>
+      <h2>两性用户种类偏好：</h2>
+      <table class="chart-container">
+            <thead>
+              <tr>
+                <th>性别</th>
+                <th>商品种类</th>
+                <th>排名</th>
+                <th>收藏量</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="record in sex_list">
+                <td>{{ record.sex }}</td>
+                <td>{{ record.tag }}</td>
+                <td>{{ record.rnk }}</td>
+                <td>{{ record.count}}</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+    <div class="info">
+        <h2>今年，您收藏商品种类的排名是</h2>
+        <table class="chart-container">
+            <thead>
+              <tr>
+                <th>种类名字</th>
+                <th>排名</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="record in collect_list">
+                <td>{{ record.tag }}</td>
+                <td>{{ record.rnk }}</td>
+                <td>{{ record.count }}</td>
+              </tr>
+            </tbody>
+          </table>
     </div>
   </div>
 </template>
@@ -44,11 +80,14 @@ export default {
   data() {
     return {
       top_list:[],
+      sex_list:[],
       top_id:0,
     };
   },
   created(){
     this.get_top4year();
+    this.get_top4sex();
+    //this.get_year_colllect()
   },
   methods: {
     get_top4year() {
@@ -58,7 +97,34 @@ export default {
       .then(res => {
           console.log(res.data.data);
           this.top_list = this.top_list.concat(res.data.data);
+      })
+    },
+    get_top4sex() {
+      this.$axios.post('http://localhost:8000/top_tag_for_sex',{
+            user_id:0,
+      })
+      .then(res => {
+          console.log(res.data.data);
+          this.sex_list = this.sex_list.concat(res.data.data);
+          for (let index = 0; index < this.sex_list.length; index++) {
+            if (this.sex_list[index].sex == 1) {
+                this.sex_list[index].sex = '男';
+            }
+            else{
+                this.sex_list[index].sex = '女';
+            }
+            
+          }
           
+      })
+    },
+    get_year_colllect(){
+        this.$axios.post('http://localhost:8000/get_collect_tag',{
+            user_id:window.localStorage.getItem("user_id"),
+      })
+      .then(res => {
+          console.log(res.data.data);
+          this.collect_list = this.collect_list.concat(res.data.data);          
       })
     }
   }
@@ -96,6 +162,13 @@ export default {
   margin-bottom: 16px;
 }
 
+.info{
+    display: block;
+}
+.info h2{
+    margin-top: 40px;
+    margin-left: 200px;
+}
 input {
   padding: 8px;
 }
@@ -106,10 +179,5 @@ button {
   color: #fff;
   border: none;
   cursor: pointer;
-}
-
-.product-list {
-  justify-content: space-between;
-  width: 100%;
 }
 </style>
